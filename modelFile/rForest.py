9feefd7a8a4dc52b7b32c055cab2acffeb6771d9
Piyush Zambre <117366588+Piyuz-89@users.noreply.github.com>
2023-10-14 12:41:01 +0530

    Update rForest.py

--- modelFile/rForest.py
+++ modelFile/rForest.py
@@ -10,7 +10,7 @@ data = pd.read_csv("car_data.csv")
 columns_to_drop = ["Name", "Power","Seats"]
 data.drop(columns=columns_to_drop, inplace=True)
 
-data = pd.get_dummies(data, columns=["Brand", "Fuel_Type", "Transmission", "Owner_Type"])
+data = pd.get_dummies(data, columns=["Brand", "Fuel_Type", "Transmission", "Owner_Type","Model Name"])
 
 
 cityData = {
