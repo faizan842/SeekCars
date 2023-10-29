64ea76e3fe53cb54df7c15807dbbdbf9e441c7a9
Piyuz89 <piyushzambre08@gmail.com>
2023-10-29 21:12:00 +0530

    model parameter update

--- modelFile/rForest.py
+++ modelFile/rForest.py
@@ -7,7 +7,7 @@ import pickle
 
 data = pd.read_csv("car_data.csv")
 
-columns_to_drop = ["Name", "Power","Seats"]
+columns_to_drop = ["Name", "Power","Seats","Model Name"]
 data.drop(columns=columns_to_drop, inplace=True)
 
 data = pd.get_dummies(data, columns=["Brand", "Fuel_Type", "Transmission", "Owner_Type"])
@@ -40,22 +40,20 @@ data['Engine'] = data['Engine'] / 100
 data = data.astype(int)
 print(data.head())
 
-
-
-
 X = data[['Kilometers_Driven', 'Mileage', 'Engine', 'New_Price', 'Location', 'Brand_Audi', 'Brand_BMW', 'Brand_Ford', 'Brand_Honda', 'Brand_Hyundai', 'Brand_Mahindra', 'Brand_Maruti', 'Brand_Mercedes', 'Brand_Nissan', 'Brand_Porsche', 'Brand_Renault', 'Brand_Skoda', 'Brand_Tata', 'Brand_Toyota', 'Brand_Volkswagen', 'Fuel_Type_Diesel', 'Fuel_Type_Petrol', 'Transmission_Manual', 'Owner_Type_Second', 'Owner_Type_Third', 'Age']]
 
 y = data['Price']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+model = RandomForestRegressor(n_estimators=100, random_state=42, max_depth=13)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-mse = mean_squared_error(y_test, y_pred)
+    
 r2 = r2_score(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
 
 print("Mean Squared Error:", mse)
 print("R-squared:", r2)
